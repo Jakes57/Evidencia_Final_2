6268cbb52d0a1219c939fd7d6621a6e208b4f5aa
Jakes57 <108290085+Jakes57@users.noreply.github.com>
2022-07-18 14:29:16 -0500

    Se agrego Switch y Do-While

--- src/main/java/com/mycompany/evidencia_final_2/Evidencia_Final_2.java
+++ src/main/java/com/mycompany/evidencia_final_2/Evidencia_Final_2.java
@@ -6,6 +6,7 @@
 package com.mycompany.evidencia_final_2;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  *
@@ -14,8 +15,26 @@ import java.io.IOException;
 public class Evidencia_Final_2 {
 
     public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
         Methods myMethods = new Methods();
         
-        myMethods.Paciente();
+        System.out.println("--BIENVENIDO AL PROGRAMA DE CITAS Y DIRECTORIO MEDICO--");
+        System.out.println("1. Crear un paciente");
+        System.out.println("2. Crear un medico");
+        System.out.println("3. Crear una cita");
+        System.out.println("4. Salir");
+        System.out.println("Por favor digite el numero de la funcion deseada:");
+        int funcion = scanner.nextInt();
+        
+        
+        do{
+        switch(funcion){
+            case 1 -> myMethods.Paciente();
+            case 2 -> myMethods.Doctor();
+            case 3 -> myMethods.Citas();
+            case 4 -> System.out.println("Tenga un buen dia.");
+        }     
+        }while (funcion !=4);
+        
     }
 }
